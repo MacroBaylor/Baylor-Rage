063102b8b0399cea066db127148a8dd078a14d98
MacroBaylor <mailamgiau01@gmail.com>
2025-04-15 18:08:52 +0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,19 +1,25 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+const bgImage = new Image();
+bgImage.src = "background.png";
+
 let player = { x: 400, width: 64, height: 64 };
 let coins = [];
 let coinCount = 0;
 let coinSpeed = 4;
-let superSaiyan = false;
+let stage = 0;
 
+let playerStages = [
+  "character_ss1.png",
+  "character_ss2.png",
+  "character_ss3.png"
+];
 let playerImg = new Image();
-playerImg.src = "character.png";
-let superSaiyanImg = new Image();
-superSaiyanImg.src = "character.png"; // Replace with another image if you have a super saiyan version
+playerImg.src = playerStages[0];
 
 let coinImg = new Image();
-coinImg.src = "coin.png";
+coinImg.src = "coin_clean.png";
 
 canvas.addEventListener("mousemove", e => {
   const rect = canvas.getBoundingClientRect();
@@ -21,27 +27,23 @@ canvas.addEventListener("mousemove", e => {
 });
 
 function spawnCoin() {
-  coins.push({ x: Math.random() * (canvas.width - 20), y: -20, size: 32 });
+  coins.push({ x: Math.random() * (canvas.width - 32), y: -32, size: 32 });
 }
 
 function update() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
 
-  // Background
-  ctx.fillStyle = "#222";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  if (coinCount >= 20) stage = 2;
+  else if (coinCount >= 10) stage = 1;
+  playerImg.src = playerStages[stage];
 
-  // Draw player
-  const img = superSaiyan ? superSaiyanImg : playerImg;
-  ctx.drawImage(img, player.x, canvas.height - player.height - 10, player.width, player.height);
+  ctx.drawImage(playerImg, player.x, canvas.height - player.height - 10, player.width, player.height);
 
-  // Draw coins
   for (let coin of coins) {
     coin.y += coinSpeed;
     ctx.drawImage(coinImg, coin.x, coin.y, coin.size, coin.size);
   }
 
-  // Coin collision detection
   coins = coins.filter(coin => {
     if (
       coin.y + coin.size >= canvas.height - player.height - 10 &&
@@ -50,13 +52,11 @@ function update() {
     ) {
       coinCount++;
       coinSpeed = 4 + coinCount * 0.2;
-      if (coinCount >= 10) superSaiyan = true;
       return false;
     }
     return coin.y < canvas.height;
   });
 
-  // Draw coin counter
   ctx.fillStyle = "white";
   ctx.font = "24px Arial";
   ctx.fillText("Coins: " + coinCount, 10, 30);
@@ -65,4 +65,6 @@ function update() {
 }
 
 setInterval(spawnCoin, 1000);
-update();
+bgImage.onload = () => {
+  update();
+};
